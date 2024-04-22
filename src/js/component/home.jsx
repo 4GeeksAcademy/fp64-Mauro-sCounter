d42a33ba65a6210dbb9271ad2e7ba4aea47e6344
Mauros721 <157764840+Mauros721@users.noreply.github.com>
2024-04-22 11:52:46 +0000

    second act

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,12 +1,14 @@
+import React, { useState, useEffect } from 'react';
+
 const SecondsCounter = () => {
     const [count, setCount] = useState(0);
  
     useEffect(() => {
         const interval = setInterval(() => {
-            setCount(count + 1);
+            setCount(prevCount => prevCount + 1); 
         }, 1000);
         return () => clearInterval(interval);
-    }, [count]);
+    }, []); 
     return (
         <div className="text-center">
             <h1>{count}</h1>
